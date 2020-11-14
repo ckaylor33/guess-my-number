4e6dd15e8e32d5389212de3ddcb8cffddd17ffa8
ckaylor33 <kaylor33@hotmail.co.uk>
2020-11-14 08:55:25 +0000

    reset game functionality

--- script.js
+++ script.js
@@ -10,10 +10,8 @@
 // document.querySelector(`.guess`).value = 23;
 // console.log(document.querySelector(`.guess`).value);
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1; //state variable
+let secretNumber = Math.trunc(Math.random() * 20) + 1; //state variable
 let score = 20; //state variable
-
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector(`.check`).addEventListener(`click`, () => {
   const guess = Number(document.querySelector(`.guess`).value);
@@ -28,6 +26,7 @@ document.querySelector(`.check`).addEventListener(`click`, () => {
     document.querySelector(`.message`).textContent = `🎉 Correct Number!`;
     document.querySelector(`body`).style.backgroundColor = `#60b347`;
     document.querySelector(`.number`).style.width = `30rem`;
+    document.querySelector('.number').textContent = secretNumber;
   } else if (guess > secretNumber) {
     //when guess is too high
     if (score > 1) {
@@ -50,3 +49,14 @@ document.querySelector(`.check`).addEventListener(`click`, () => {
     }
   }
 }); //function gets called when event happens
+
+document.querySelector(`.again`).addEventListener(`click`, () => {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector(`.message`).textContent = `Start guessing...`;
+  document.querySelector(`.guess`).value = ``;
+  document.querySelector(`body`).style.backgroundColor = `#222`;
+  document.querySelector(`.number`).style.width = `15rem`;
+  document.querySelector(`.number`).textContent = `?`;
+  document.querySelector(`.score`).textContent = score;
+});
