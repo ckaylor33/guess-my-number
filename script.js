5c7340bd4665cc63aacd14d847591c116c33524a
ckaylor33 <kaylor33@hotmail.co.uk>
2020-11-14 07:57:08 +0000

    game logic

--- script.js
+++ script.js
@@ -10,6 +10,11 @@
 // document.querySelector(`.guess`).value = 23;
 // console.log(document.querySelector(`.guess`).value);
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1; //state variable
+let score = 20; //state variable
+
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector(`.check`).addEventListener(`click`, () => {
   const guess = Number(document.querySelector(`.guess`).value);
   console.log(guess, typeof guess);
@@ -17,5 +22,25 @@ document.querySelector(`.check`).addEventListener(`click`, () => {
   if (!guess) {
     //first scenario always assume no input
     document.querySelector(`.message`).textContent = `⛔️ No Number!`;
+  } else if (guess === secretNumber) {
+    document.querySelector(`.message`).textContent = `🎉 Correct Number!`;
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(`.message`).textContent = `📈 Too high!`;
+      score--;
+      document.querySelector(`.score`).textContent = score;
+    } else {
+      document.querySelector(`.message`).textContent = `💥 You lost the game!`;
+      document.querySelector(`.score`).textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(`.message`).textContent = `📉 Too low!`;
+      score--;
+      document.querySelector(`.score`).textContent = score;
+    } else {
+      document.querySelector(`.message`).textContent = `💥 You lost the game!`;
+      document.querySelector(`.score`).textContent = 0;
+    }
   }
 }); //function gets called when event happens
